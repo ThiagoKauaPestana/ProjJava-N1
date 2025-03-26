aa7da65599c78e36ddc6aac923d8cbcf137b8e11
Thiago Kauã Pestana <145944462+ThiagoKauaPestana@users.noreply.github.com>
2025-03-26 23:20:59 +0000

    Fazendo Opção 1 e 2

--- ProjJava/src/App.java
+++ ProjJava/src/App.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-
+import java.util.Random; // Import da função que cria um vetor aleatorio
 
 public class App {
     public static void main(String[] args) {
         
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in); // Declaração Scanner
+        int[] vetor; // Declaração vetor
         boolean VerficaVetor = false; // Verificador do Vetor
         int op; // variavel da opcao do menu
-
+        Random aleatorio = new Random(); // Declarando Random
 
         do{ // REPETIR ATE SAIR DO LACO (APERTAR NO 9. SAIR)
             System.out.println("1. Inicializar o vetor com números aleatórios");
@@ -27,13 +28,28 @@ public class App {
             // SWITCH CASE PARA LER A OPCAO SOLICITADA PELO USUARIO E EXECUTAR O METODO STATIC
             switch (op) {
                 case 1:
-
-                    System.out.println("CAIU NA 1"); // Inicializar o vetor com números aleatórios
+                    // Inicializar o vetor com números aleatórios
+                    // Nao necessario fazer metodo/funcao
+                
+                    System.out.print("Digite o tamanho do Vetor: "); 
+                    int n = entrada.nextInt(); 
+                    System.out.print("Digite o maior numero do Vetor (Inteiro Positivo): ");
+                    int m = entrada.nextInt();
+                    
+                    vetor = new int[n]; // Inicializando o vetor N posições
+                    for (int i = 0; i < n; i++) { // Gerando um numero aleatorio para cada posição do vetor
+                        vetor[i] = aleatorio.nextInt(m + 1); // Gerando um numero pra posição I garantindo que o intervalo de 0 até o numero M
+                    }
+                    System.out.print("Vetor inicializado, liberando as outras opções do Sistema");
                     VerficaVetor = true; // MUDANDO A VARIAVEL BOLEANA PARA TRUE, ASSIM OS OUTROS CASOS PODEM SER EXECUTADOS
                     break;
+
                 case 2:
                     if (VerficaVetor) { // VERIFICACAO SE O VETOR JA FOI CRIADO, SO E EXECUTADA SE A VARIAVEL SER TRUE
-                        System.out.println("Caiu na 2"); // Imprimir o vetor
+                       
+                        for(int i = 0; i < m; i++);
+                            System.out.print();
+
                     } else { // CASO VERIFICACAO SER FALSE EXECUTAR ELSE
                         System.out.println("caiu na 2 porem nao pode ser usada");
                     }
