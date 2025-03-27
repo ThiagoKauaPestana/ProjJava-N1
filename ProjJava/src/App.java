7436cb2b5f59fabb2cc3da31f37019d33e810dc4
Thiago Kauã Pestana <145944462+ThiagoKauaPestana@users.noreply.github.com>
2025-03-27 00:08:50 +0000

    Opcao 1 e 2 Finalizada

--- ProjJava/src/App.java
+++ ProjJava/src/App.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 import java.util.Random; // Import da função que cria um vetor aleatorio
 
 public class App {
+
+    
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in); // Declaração Scanner
-        int[] vetor; // Declaração vetor
+        int[] vetor = null; // Declaração vetor
         boolean VerficaVetor = false; // Verificador do Vetor
         int op; // variavel da opcao do menu
         Random aleatorio = new Random(); // Declarando Random
-
+        
         do{ // REPETIR ATE SAIR DO LACO (APERTAR NO 9. SAIR)
             System.out.println("1. Inicializar o vetor com números aleatórios");
             System.out.println("2. Imprimir o vetor");
@@ -20,7 +22,7 @@ public class App {
             System.out.println("7. Calcula a média centralizada dos números armazenados no vetor");
             System.out.println("8. Verificar se dado um valor existe dois números em posições distintas que somados são iguais ao valor informado.");
             System.out.println("9. Sair");
-
+            
             // ENTRADA DA OPCAO QUE O USARIO QUER UTILIZAR
             System.out.print("Digite a Opção: ");
             op = entrada.nextInt();
@@ -30,7 +32,7 @@ public class App {
                 case 1:
                     // Inicializar o vetor com números aleatórios
                     // Nao necessario fazer metodo/funcao
-                
+                    
                     System.out.print("Digite o tamanho do Vetor: "); 
                     int n = entrada.nextInt(); 
                     System.out.print("Digite o maior numero do Vetor (Inteiro Positivo): ");
@@ -43,13 +45,10 @@ public class App {
                     System.out.print("Vetor inicializado, liberando as outras opções do Sistema");
                     VerficaVetor = true; // MUDANDO A VARIAVEL BOLEANA PARA TRUE, ASSIM OS OUTROS CASOS PODEM SER EXECUTADOS
                     break;
-
+                    
                 case 2:
                     if (VerficaVetor) { // VERIFICACAO SE O VETOR JA FOI CRIADO, SO E EXECUTADA SE A VARIAVEL SER TRUE
-                       
-                        for(int i = 0; i < m; i++);
-                            System.out.print();
-
+                        printar(vetor);
                     } else { // CASO VERIFICACAO SER FALSE EXECUTAR ELSE
                         System.out.println("caiu na 2 porem nao pode ser usada");
                     }
@@ -82,35 +81,36 @@ public class App {
                         System.out.println("caiu na 6 porem nao pode ser usada");
                     }
                     break;
-
+                    
                 case 7:
                     if (VerficaVetor) { // VERIFICACAO SE O VETOR JA FOI CRIADO, SO E EXECUTADA SE A VARIAVEL SER TRUE
                         System.out.println("Caiu na 7"); // Calcula a média centralizada dos números armazenados no vetor
-                    } else { // CASO VERIFICACAO SER FALSE EXECUTAR ELSE
-                        System.out.println("caiu na 7 porem nao pode ser usada");
-                    }
-                    break;
-                case 8:
-                    if (VerficaVetor) { // VERIFICACAO SE O VETOR JA FOI CRIADO, SO E EXECUTADA SE A VARIAVEL SER TRUE
-                        System.out.println("Caiu na 8"); // Verificar se dado um valor existe dois números em posições distintas que somados são iguais ao valor informado.
-                    } else { // CASO VERIFICACAO SER FALSE EXECUTAR ELSE
-                        System.out.println("caiu na 8 porem nao pode ser usada");
-                    }
-                    break;
-                case 9:
-                    System.out.println("Caiu na 9 SAIRRR"); // SAIR
-                    break;
-                default:
-                    System.out.println("Opção inválida, tente novamente.");
+                        } else { // CASO VERIFICACAO SER FALSE EXECUTAR ELSE
+                            System.out.println("caiu na 7 porem nao pode ser usada");
+                        }
+                        break;
+                    case 8:
+                        if (VerficaVetor) { // VERIFICACAO SE O VETOR JA FOI CRIADO, SO E EXECUTADA SE A VARIAVEL SER TRUE
+                            System.out.println("Caiu na 8"); // Verificar se dado um valor existe dois números em posições distintas que somados são iguais ao valor informado.
+                        } else { // CASO VERIFICACAO SER FALSE EXECUTAR ELSE
+                            System.out.println("caiu na 8 porem nao pode ser usada");
+                        }
+                        break;
+                    case 9:
+                        System.out.println("Caiu na 9 SAIRRR"); // SAIR
+                        break;
+                        default:
+                        System.out.println("Opção inválida, tente novamente.");
+                }
+                // CONTINUA NO LACO SE FOR DIFERENTE DE 9
+            } while (op != 9);
+                
+            entrada.close();
+        }
+        public static void printar(int vetor[]) {
+            for(int i = 0; i < vetor.length; i++) {
+                System.out.println(vetor[i] + " ");
             }
-            // CONTINUA NO LACO SE FOR DIFERENTE DE 9
-        } while (op != 9);
-
-
-
-
-
-        entrada.close();
-    }
+        }
 }
  
